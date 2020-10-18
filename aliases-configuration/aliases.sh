8e0abca8908ca05a14fd6666875bafeb0e7af1d1
Florent Clarret <florent.clarret@gmail.com>
2020-10-18 19:24:22 +0200

    refactor(docker): move the docker alias into a specific file

--- aliases-configuration/aliases.sh
+++ aliases-configuration/aliases.sh
@@ -67,21 +67,6 @@ fi
 # tar
 alias untar='tar -xvf'
 
-# docker
-alias dk='docker'
-# Get latest container ID
-alias dkl="docker ps -l -q"
-# Get container process
-alias dkps="docker ps"
-# Get process included stop container
-alias dkpsa="docker ps -a"
-# Get images
-alias dki="docker images"
-# Execute interactive container, e.g., $dex base /bin/bash
-alias dkex="docker exec -i -t"
-# Stop and Remove all containers
-alias dkrmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-
 # tmux
 alias tmuxlast='tmux attach || tmux new-session'
 
--- aliases-configuration/load.sh
+++ aliases-configuration/load.sh
@@ -5,6 +5,7 @@ ALIAS_FOLDER="$HOME/.aliases/"
 source "$ALIAS_FOLDER/git/git.sh"
 source "$ALIAS_FOLDER/screen/screen.sh"
 source "$ALIAS_FOLDER/borg/borg-aliases.bash"
+source "$ALIAS_FOLDER/docker/docker-aliases.bash"
 
 source "$ALIAS_FOLDER/ssh/start-agent.sh"
 
