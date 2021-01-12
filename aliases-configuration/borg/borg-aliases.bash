53eb96f86e19c64473e3d8775494d3a8706068d4
Florent Clarret <florent.clarret@gmail.com>
2021-01-12 17:46:17 +0100

    feat(borg): exclude vm folder

--- aliases-configuration/borg/borg-aliases.bash
+++ aliases-configuration/borg/borg-aliases.bash
@@ -2,4 +2,4 @@
 
 # Some aliases for my personal manual backup that I use occasionally
 alias borg-home="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude /home/florentclarret/.sync /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
-alias borg-home-no-music="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude /home/florentclarret/.sync --exclude /home/florentclarret/Music /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
+alias borg-home-no-music="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude /home/florentclarret/.sync --exclude /home/florentclarret/Music --exclude \"/home/florentclarret/VirtualBox VMs\" /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
