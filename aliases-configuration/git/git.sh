2b01def6cf2b36debbb08ef4a192dec538819788
Florent Clarret <florent.clarret@gmail.com>
2021-02-20 17:58:46 +0100

    feat(git): add a function to init my remotes

--- aliases-configuration/git/git.sh
+++ aliases-configuration/git/git.sh
@@ -3,3 +3,13 @@
 get_git_current_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' | sed -e 's/* \(.*\)/ (branch: \1) /'
 }
+
+init_remote() {
+    for remote in origin gitlab github; do
+      git remote rm $remote
+    done;
+
+    git remote add origin "ssh://private-gitlab/florent.clarret/$1.git"
+    git remote add gitlab "git@gitlab.com:florent.clarret/$1.git"
+    git remote add github "git@github.com:FlorentClarret/$1.git"
+}
